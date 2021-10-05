86175c3ab8d65afd96b1729cf6677460caaa319f
scarter31191 <scarter31191@gmaile.com>
2021-10-05 19:37:31 -0400

    used the only param to hide timestap and password data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    render json: @users, only: [:name, :username, :email]
   end
 
   # GET /users/1
