0da0bbbced165ceb590787778bb36ecb22911192
scarter31191 <scarter31191@gmaile.com>
2021-10-09 22:50:29 -0400

    refactored blog routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
-  resources :blogs
   namespace :api do
     namespace :v1 do
       resources :users do
         resources :goals
         resources :job_opportunities
+        resources :blogs
       end
     end
   end
